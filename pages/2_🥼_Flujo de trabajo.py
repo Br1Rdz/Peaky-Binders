ae9d9d538056ad228dbf75a7e41f06fd3062feaa
Br1Rdz <91506187+Br1Rdz@users.noreply.github.com>
2025-06-22 13:24:55 -0600

    Update 2_🥼_Flujo de trabajo.py

--- pages/2_🥼_Flujo de trabajo.py
+++ pages/2_🥼_Flujo de trabajo.py
@@ -17,6 +17,12 @@ hide_st_style = '''
 st.markdown(hide_st_style, unsafe_allow_html= True)
 st.logo("./Informacion.png", icon_image="./info2.png")
 
-st.markdown("<h1 style='text-align: center; color: white;'>Flujo de trabajo</h1>", unsafe_allow_html=True)
+# st.markdown("<h1 style='text-align: center; color: white;'>Flujo de trabajo</h1>", unsafe_allow_html=True)
+st.markdown("""
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Stardos+Stencil:wght@700&display=swap" rel="stylesheet">
+<h1 style='text-align: center; font-family: "Stardos Stencil", serif; font-size: 56px; color: white; letter-spacing: 2px;'>
+Resultados
+</h1>
+""", unsafe_allow_html=True)
 
 st.image("data/Diagrama.png", use_column_width=True)
